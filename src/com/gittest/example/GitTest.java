1196314c029ead2183bc7047bd3ed5af2e9f8bec
LEonardoELF <leonardo_turtles@rambler.ru>
2021-03-17 15:50:39 +0500

    test commit

--- src/com/gittest/example/GitTest.java
+++ src/com/gittest/example/GitTest.java
@@ -3,5 +3,8 @@ package com.gittest.example;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Превед Мир");
+        GitTest2 gt2=new GitTest2();
+        gt2.writeSmth();
+        System.out.println("123");
     }
 }
--- src/com/gittest/example/GitTest2.java
+++ src/com/gittest/example/GitTest2.java
@@ -1,2 +1,7 @@
-package com.gittest.example;public class GitTest2 {
+package com.gittest.example;
+
+public class GitTest2 {
+    public void writeSmth(){
+        System.out.println("Hello World!!!");
+    }
 }
